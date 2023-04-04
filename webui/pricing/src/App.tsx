488a01e0587b80ffdc9f3707ecb283491a2a0f0a
rahgadda <gaddam.rahul@gmail.com>
2023-04-04 17:51:13 +0000

    Initial Draft

--- webui/pricing/src/App.tsx
+++ webui/pricing/src/App.tsx
@@ -3,7 +3,27 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello world!</h1> 
+    <div className="text-center">
+      <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello world!</h1>
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th className="px-4 py-2 border">Type</th>
+            <th className="px-4 py-2 border">Description</th>
+            <th className="px-4 py-2 border">Enable</th>
+            <th className="px-4 py-2 border">System Defined</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="px-4 py-2 border">gender</td>
+            <td className="px-4 py-2 border">Gender</td>
+            <td className="px-4 py-2 border">Y</td>
+            <td className="px-4 py-2 border">N</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
